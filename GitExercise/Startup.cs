ccbe6afab152576d1f25c7c1a24a2f20fa554aa9
IT-Career <nikolai05ivanov@gmail.com>
2024-02-15 17:03:50 +0200

    Division, Abs Subtraction added; Introduced authorization

--- GitExercise/Startup.cs
+++ GitExercise/Startup.cs
@@ -6,6 +6,15 @@ namespace GitExercise
     {
         public static void Main()
         {
+
+            bool isAuthorized = CheckCredentials();
+
+            if(!isAuthorized)
+            {
+                Console.WriteLine("Access Denied.");
+                Console.ReadKey(intercept: true);
+                return;
+            }
             Console.WriteLine("Console Calculator App");
             Console.WriteLine(new string('-', 15));
 
@@ -35,10 +44,26 @@ namespace GitExercise
                 case "m":
                     OptionsManager.Multiply(a, b);
                     break;
+                case "d":
+                    OptionsManager.Divide(a, b);
+                    break;
+                case "sabs":
+                    OptionsManager.SubtractAbs(a, b);
+                    break;
             }
 
             Console.WriteLine("Pres any key to close the app...");
             Console.ReadKey(true);
         }
+        private static bool CheckCredentials()
+        {
+            Console.Write("Enter password to gain access: ");
+            string password = Console.ReadLine();
+            Console.Clear();
+
+            return password == Password;
+        }
+
+        private const string Password = "fortnite";
     }
 }
